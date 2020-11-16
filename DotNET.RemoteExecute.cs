bb8880048c3e0ef432ebefc6cfc02a7f64e6ca17
Jacob C. Ting <JCobTing@gmail.com>
2020-11-16 18:34:18 +0000

    Outline StartAsRemote Class

---
+++ DotNET.RemoteExecute.cs
@@ -0,0 +1,31 @@
+﻿namespace DotNET.RemoteExecute
+{
+    public class StartAsRemote
+    {
+        private string procName;
+        public string ProcName
+        {
+            get { return procName; }
+            set { procName = value; }
+        }
+        public StartAsRemote(string procName)
+        {
+            this.procName = procName;
+        }
+        public void WithAll()
+        {
+            // Try WMI
+            // Try PSExec
+            // Try Scheduled Task API
+        }
+        public void WithWMI()
+        {
+        }
+        public void WithPSExec()
+        {
+        }
+        public void WithSchTaskAPI()
+        {
+        }
+    }
+}
